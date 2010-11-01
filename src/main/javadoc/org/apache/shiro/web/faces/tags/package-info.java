33092caf8751473d861af383458788ad294946e2
Deluan <github@deluan.com.br>
2010-11-01 08:11:57 -0200

    more javadocs

---
+++ src/main/javadoc/org/apache/shiro/web/faces/tags/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Provides the Shiro Facelets Tag Library implementations.
+ *
+ * <p>Shiro Facelets Tags can be used to evalute or not evaluate (show or not show) parts of a Facelets page
+ * based on the current user's authentication status and/or authorization (access control) abilities.</p>
+ *
+ * <p>Based on the original Shiro JSP tags</p>
+ *
+ * <p>OBS: These tags are meant to be used with JSF 1.X and Facelets 1.1.X. They will NOT work with JSF 2.</p>
+ * @author Deluan
+ */
+package org.apache.shiro.web.faces.tags;
+
+
