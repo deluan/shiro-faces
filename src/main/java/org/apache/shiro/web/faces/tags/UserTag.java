a2ef89437ae2b29c983ba26e197755bd872891ce
Deluan <github@deluan.com.br>
2010-11-18 12:35:21 -0200

    added @Override

--- src/main/java/org/apache/shiro/web/faces/tags/UserTag.java
+++ src/main/java/org/apache/shiro/web/faces/tags/UserTag.java
@@ -5,15 +5,16 @@ import com.sun.facelets.tag.TagConfig;
 /**
  * Tag that renders the tag body if the current user known to the system, either from a successful login attempt
  * (not necessarily during the current session) or from 'RememberMe' services.
- *
+ * <p/>
  * <p><b>Note:</b> This is <em>less</em> restrictive than the <code>AuthenticatedTag</code> since it only assumes
  * the user is who they say they are, either via a current session login <em>or</em> via Remember Me services, which
  * makes no guarantee the user is who they say they are.  The <code>AuthenticatedTag</code> however
  * guarantees that the current user has logged in <em>during their current session</em>, proving they really are
  * who they say they are.
- *
+ * <p/>
  * <p>The logically opposite tag of this one is the {@link GuestTag}.
- **
+ * *
+ *
  * @author Deluan Quintao
  * @author Les Hazlewood
  */
@@ -23,6 +24,7 @@ public class UserTag extends GuestTag {
         super(config);
     }
 
+    @Override
     protected boolean checkAuthentication() {
         return !super.checkAuthentication();
     }
